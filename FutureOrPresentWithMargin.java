ad10f72dad4fab2c1fa157d01a6ef3e17a835bb0
issanyo <ihsanyounes90@gmail.com>
2020-08-13 12:16:08 +0100

    Create FutureOrPresentWithMargin.java

---
+++ FutureOrPresentWithMargin.java
@@ -0,0 +1,34 @@
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = {FutureOrPresentWithMarginValidator.class})
+public @interface FutureOrPresentWithMargin {
+    String message() default "{javax.validation.constraints.FutureOrPresent.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * Margin of error in seconds
+     * @return
+     */
+    int margin() default 600;
+
+    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public @interface List {
+        FutureOrPresentWithMargin[] value();
+    }
+}
